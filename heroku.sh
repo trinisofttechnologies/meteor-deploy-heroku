9b7a17ddb0aa9f7e6d5d8cd527db9d60f97fda39
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2017-03-08 12:25:27 +0530

    mongo and node updated

--- heroku.sh
+++ heroku.sh
@@ -1,11 +1,11 @@
 ##### DEPLOY VARIABLES #######
 HEROKU_APP='shooterapp'
 ROOT_URL='https://'$HEROKU_APP'.herokuapp.com/'
-MONGO_URL='mongodb://leoquip.com:123456@paulo.mongohq.com:10017/youtap'
-MONGO_OPLOG_URL='mongodb://leoquip.com:123456@paulo.mongohq.com:10017/youtap'
+MONGO_URL='mongodb://trinisoft:trinisoft@ds123410.mlab.com:23410/development'
+MONGO_OPLOG_URL='mongodb://trinisoft:trinisoft@ds123410.mlab.com:23410/development'
 MAIL_URL=''
 METEOR_SETTINGS=$(cat ./settings.json)
-nodeVersion="0.10.36" ## not all node version works with some meteor version
+nodeVersion="4.7.3" ## not all node version works with some meteor version
 
 ##### DELETE A LINE IF YOU DON"T WANT SOME ENV VAR #######
 runSript="cd ./programs/server &&
